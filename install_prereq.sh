e11d75cd9c6433b3df3eb22418fcca4ffff6fe9c
Ben <peacepenguin@users.noreply.github.com>
2026-01-03 00:33:46 -0700

    fix debian 13 build
    allowing override for breaking system packages. pointed sslpsk to fork of upstreams ssl-context refactor fixes.

--- install_prereq.sh
+++ install_prereq.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 debianInstall() {
 	sudo apt-get update
-	sudo apt-get install -y git iw dnsmasq rfkill hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev iproute2 iputils-ping
-	sudo python3 -m pip install --user --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git pycryptodomex
+	sudo apt-get install -y git iw dnsmasq rfkill hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev iproute2 iputils-ping python3-paho-mqtt python3-tornado
+	sudo python3 -m pip install --break-system-packages --upgrade git+https://github.com/peacepenguin/sslpsk.git pycryptodomex
 }
 
 archInstall() {
